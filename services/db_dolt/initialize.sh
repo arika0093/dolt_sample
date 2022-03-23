ff9947e4fa7bd128cbe71c49902b8a3bd847b619
arika <delete0093@gmail.com>
2022-03-23 20:54:18 +0900

    initialize-shell fixed

--- services/db_dolt/initialize.sh
+++ services/db_dolt/initialize.sh
@@ -1,8 +1,8 @@
 # initialize repository
 echo "Initializing repository..."
-init_result = $(dolt init)
+result=$(dolt init)
 if [ $? -ne 0 ]; then
 	echo "dolt is already initialized."
 	exit 0
-fi
+fi 
 
